48f303b1c97ad1cf06e46cce126a19cb044ff123
zoltan2561 <pappzoltan6969@gmail.com>
2024-07-09 22:08:01 +0200

    elso

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize('secret_server', 'root', 'zolizoli', {
     host: 'localhost',
     dialect: 'mysql'
 });
-
+//a
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
