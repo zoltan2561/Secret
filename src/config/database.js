f91c7138a3644b0735c96a9c4c53ce62395dbebc
zoltan2561 <pappzoltan6969@gmail.com>
2024-07-10 10:40:17 +0200

    teszt

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
-
+//TODO:github secretben traolni
 const sequelize = new Sequelize('secret_server', 'root', 'zolizoli', {
     host: 'localhost',
     dialect: 'mysql'
 });
-//a
+
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
